075fc374c02aa99db2a6309762c1411d7e9762c4
William Spear <spearw@gmail.com>
2018-11-14 16:44:40 -0500

    reverted previous change

--- config/application.rb
+++ config/application.rb
@@ -1,23 +1,7 @@
 require_relative 'boot'
 
-require "rails"
-
-# Include each railties manually, excluding `active_storage/engine`
-%w(
-  active_record/railtie
-  action_controller/railtie
-  action_view/railtie
-  action_mailer/railtie
-  active_job/railtie
-  action_cable/engine
-  rails/test_unit/railtie
-  sprockets/railtie
-).each do |railtie|
-  begin
-    require railtie
-  rescue LoadError
-  end
-end
+require "rails/all"
+require "active_storage"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
